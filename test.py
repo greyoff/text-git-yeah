1a737dad78f9ee7a1165f1384623fce9fe99934d
greyoff <greyoff@icloud.com>
2022-04-03 01:00:49 +0800

    Update test.py

--- test.py
+++ test.py
@@ -9,8 +9,12 @@ def some_function(a, b):
 
 print(some_function(3, 4))
 
+def sb_func(x, y, z):
+    print("I'm SB!!!!!!")
+
 def main(a, b, c):
     return some_function(a, b) + c
 
 if __name__ == "__main__":
+    sb_func("d", "s", "b")
     main(1, 2, 3)
